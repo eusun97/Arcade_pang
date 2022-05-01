86991059246d621a3794c66b5aad8da863cbc34f
Yuseon Lee <usun1128@naver.com>
2022-05-01 14:08:41 +0900

    ball_movement commit

--- pygame_arcade/arcade_pang.py
+++ pygame_arcade/arcade_pang.py
@@ -47,7 +47,7 @@ character_speed = 5
 # 무기 만들기
 weapon = pygame.image.load(os.path.join(image_path, "weapon.png"))
 weapon_size = weapon.get_rect().size
-weapon_width = weapon_size[0] # width만 필요한 이유: 무기 쏠 때 캐릭터 위치 기준 중앙에서 나오기 위해
+weapon_width = weapon_size[0] 
 
 # 무기는 한번에 여러 발 발사 가능
 weapons =  []
@@ -55,6 +55,29 @@ weapons =  []
 # 무기 이동 속도
 weapon_speed = 10
 
+# 공 만들기 (4개 크기에 대해 따로 처리)
+ball_images =[
+    pygame.image.load(os.path.join(image_path, "balloon1.png")),
+    pygame.image.load(os.path.join(image_path, "balloon2.png")),
+    pygame.image.load(os.path.join(image_path, "balloon3.png")),
+    pygame.image.load(os.path.join(image_path, "balloon4.png"))
+]
+
+# 공 크기에 따른 최초 스피드
+ball_speed_y = [-18, -15, -12, -9] # 각 index 0,1,2,3에 해당하는 값
+
+# 공들
+balls = []
+
+# 최초 발생하는 큰 공 추가
+balls.append({
+    "pos_x" : 50,   #  공의 x 좌표
+    "pos_y" : 50,   #  공의 y 좌표
+    "img_idx" : 0,  # 공의 이미지 인덱스
+    "to_x" : 3,     # 공의 x축 이동 방향, -3이면 왼쪽, 3이면 오른쪽
+    "to_y" : -6,    # y축 이동향뱡
+    "init_spd_y" : ball_speed_y[0] # y 최초 속도(공마다 최초 속도 다르기때문에) 
+})
 
 running = True 
 while running: 
@@ -70,7 +93,7 @@ while running:
                 character_to_x -= character_speed
             elif event.key == pygame.K_RIGHT:
                 character_to_x += character_speed
-            elif event.key == pygame.K_SPACE: # 스페이스 눌렀을때 무기 발사
+            elif event.key == pygame.K_SPACE: 
                 weapon_x_pos = character_x_pos + (character_width / 2) - (weapon_width / 2) # 발사 위치(캐릭터 정중앙)
                 weapon_y_pos = character_y_pos
                 weapons.append([weapon_x_pos, weapon_y_pos])
@@ -95,6 +118,31 @@ while running:
 # 천장에 닿은 무기 없애기
     weapons = [ [w[0], w[1]] for w in weapons if w[1] > 0]
 
+# 공 위치 정의
+    for ball_idx, ball_val in enumerate(balls): # enumerate : balls리스트에 있는걸 하나씩 가져와서 몇번재에 있는지, 그 값도 가져와줌
+        ball_pos_x = ball_val["pos_x"] # ball_val에 있는 pos_x 값을 ball_pos_x에 넣음
+        ball_pos_y = ball_val["pos_y"]
+        ball_img_idx = ball_val["img_idx"]
+
+        ball_size = ball_images[ball_img_idx].get_rect().size
+        ball_width = ball_size[0]
+        ball_height = ball_size[1]
+
+# 가로벽에 닿았을 때 공 이동 위치 변경 (반대편으로 튕겨나오는 효과)
+        if ball_pos_x < 0 or ball_pos_x > screen_width - ball_width:
+            ball_val["to_x"] = ball_val["to_x"] * -1 
+
+# 세로 위치
+# 스테이지에 튕겨 올라가는 처리
+        if ball_pos_y >= screen_height -  stage_height - ball_height:
+            ball_val["to_y"] = ball_val["init_spd_y"]
+        else: # 그 외의 모든 경우에는 속도를 증가
+            ball_val["to_y"] += 0.5 
+
+# 값을 반영 (위치 정의)
+        ball_val["pos_x"] += ball_val["to_x"]
+        ball_val["pos_y"] += ball_val["to_y"]
+
 
 ### 4. 충돌 처리 
 
@@ -105,6 +153,12 @@ while running:
 
     for weapon_x_pos, weapon_y_pos in weapons:
         screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
+
+    for idx, val in enumerate(balls):
+        ball_pos_x = val["pos_x"]
+        ball_pos_y = val["pos_y"]
+        ball_img_idx = val["img_idx"]
+        screen.blit(ball_images[ball_img_idx], (ball_pos_x, ball_pos_y))
 
     screen.blit(stage, (0, screen_height - stage_height))
     screen.blit(character, (character_x_pos, character_y_pos))
