414a50b2b16e2e2c0d508e32ad01e6f624380a48
Yuseon Lee <usun1128@naver.com>
2022-05-01 13:40:43 +0900

    weapon commit

--- pygame_arcade/arcade_pang.py
+++ pygame_arcade/arcade_pang.py
@@ -19,16 +19,16 @@ clock = pygame.time.Clock()
 ####################################################
 
 ### 1. 사용자 게임 초기화 (배경화면, 게임 이미지, 좌표, 속도, 폰트, 시간 등)
-current_path = os.path.dirname(__file__) # 현재 파일의 위치 반환해줌 , import os 필요
-image_path = os.path.join(current_path, "images") # images 폴더 위치 반환
+current_path = os.path.dirname(__file__) 
+image_path = os.path.join(current_path, "images") 
 
 # 배경 만들기
-background = pygame.image.load(os.path.join(image_path, "background.png")) # 간결하게 이미지 첨부하는 방식
+background = pygame.image.load(os.path.join(image_path, "background.png")) 
 
 # 스테이지 만들기
 stage = pygame.image.load(os.path.join(image_path, "stage.png"))
 stage_size = stage.get_rect().size
-stage_height = stage_size[1] # 스테이지 높이를 계산해서 공을 튕기거나 스테이지 높이에 캐릭터를 놓으려고
+stage_height = stage_size[1] 
 
 # 캐릭터 만들기
 character = pygame.image.load(os.path.join(image_path, "character.png"))
@@ -37,6 +37,24 @@ character_width = character_size[0]
 character_height = character_size[1]
 character_x_pos = screen_width / 2 - character_width / 2
 character_y_pos = screen_height - character_height - stage_height
+
+# 캐릭터 이동 방향
+character_to_x = 0
+
+# 캐릭터 이동 속도
+character_speed = 5
+
+# 무기 만들기
+weapon = pygame.image.load(os.path.join(image_path, "weapon.png"))
+weapon_size = weapon.get_rect().size
+weapon_width = weapon_size[0] # width만 필요한 이유: 무기 쏠 때 캐릭터 위치 기준 중앙에서 나오기 위해
+
+# 무기는 한번에 여러 발 발사 가능
+weapons =  []
+
+# 무기 이동 속도
+weapon_speed = 10
+
 
 running = True 
 while running: 
@@ -47,7 +65,35 @@ while running:
         if event.type == pygame.QUIT: 
             running = False 
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                character_to_x -= character_speed
+            elif event.key == pygame.K_RIGHT:
+                character_to_x += character_speed
+            elif event.key == pygame.K_SPACE: # 스페이스 눌렀을때 무기 발사
+                weapon_x_pos = character_x_pos + (character_width / 2) - (weapon_width / 2) # 발사 위치(캐릭터 정중앙)
+                weapon_y_pos = character_y_pos
+                weapons.append([weapon_x_pos, weapon_y_pos])
+
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                character_to_x = 0
+            
+
 ### 3. 게임 캐릭터 위치 정의
+    character_x_pos += character_to_x
+
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+# 무기 위치 조정 (위로 발사)
+    weapons = [ [w[0], w[1] - weapon_speed] for w in weapons]
+
+# 천장에 닿은 무기 없애기
+    weapons = [ [w[0], w[1]] for w in weapons if w[1] > 0]
 
 
 ### 4. 충돌 처리 
@@ -56,8 +102,13 @@ while running:
 ### 5. 화면에 그리기
 
     screen.blit(background, (0,0))
+
+    for weapon_x_pos, weapon_y_pos in weapons:
+        screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
+
     screen.blit(stage, (0, screen_height - stage_height))
     screen.blit(character, (character_x_pos, character_y_pos))
+  
     
 
     pygame.display.update()
